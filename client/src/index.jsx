8d0fc2da70cdffb4b9f194b95aa721b25382dc00
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-04-08 19:31:39 -0700

    Add index.html to public file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,5 +14,5 @@ const AppRouter = () => (
   </Router>
 )
 
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+window.AppRouter = App;
+// ReactDOM.render(<AppRouter />, document.getElementById('app'));
