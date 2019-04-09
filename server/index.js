3f58f49075a530f377bd17ede6ad029f19e816b3
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-04-08 19:42:03 -0700

    Include dist folder in git

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = 7000;
 // app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '/../public/dist')));
+app.use(express.static(path.join(__dirname, '/../public')));
 
 app.listen(port, () => {
   console.log(`Server is now listening on port: ${port}`)
